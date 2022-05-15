1dde0df1ca8ef577925e1658f0361bd85153f791
XMMR12 <31443996+XMMR12@users.noreply.github.com>
2022-05-15 06:57:36 +0200

    The code to download any video

---
+++ downloadvideos.js
@@ -0,0 +1,5 @@
+/*Note: steps:*/
+/*1-copy the following code*/
+/*2-goto URL bar then type "javascript:" then paste then press ENTER key*/
+
+x_py=document;x_py.location=document.getElementsByTagName((+(31))["to"+String["name"]](32)+(x_py.A+"")[5]+((!+"")[0]+"")[2]+(""+[!+""])[3]+(!!1+[]["flat"])[10])[0].src
